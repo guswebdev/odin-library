4796828a45307e6340a4d607a80b86c7c99c177e
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-09-26 06:06:44 -0300

    Maquetacion y programacion del form y modal

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const d = document;
+//Prueba MODAL
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".header button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+const $form = d.getElementById("form");
 const myLibrary = [];
 
 function Book(title, autor, paginas, estado) {
@@ -16,18 +33,33 @@ addBookToLibrary("11 JG", "SSC", 100, true);
 addBookToLibrary("gp 11", "css", 500, false);
 addBookToLibrary("Cita en la Cima", "Raimon Samso", 250, false);
 
-
-
-function mostrarLibros(){
-    for(let i of myLibrary){
-        document.write(`<h4>${i.id}</h4>`)
-        document.write(`<h4>${i.title}</h4>`)
-        document.write(`<h4>${i.autor}</h4>`)
-        document.write(`<h4>${i.paginas}</h4>`)
-        document.write(`<h4>${i.estado}</h4>`)
-    }
-    console.log(myLibrary);
-    //Recorrer el array y mostrar datos
+function mostrarLibros() {
+  /*
+  for (let i of myLibrary) {
+    document.write(`<h4>${i.id}</h4>`);
+    document.write(`<h4>${i.title}</h4>`);
+    document.write(`<h4>${i.autor}</h4>`);
+    document.write(`<h4>${i.paginas}</h4>`);
+    document.write(`<h4>${i.estado}</h4>`);
+  }
+    */
+  console.log(myLibrary);
+  //Recorrer el array y mostrar datos
 }
 
-//mostrarLibros()
+mostrarLibros();
+
+const submit = (e) => {
+  e.preventDefault();
+  addBookToLibrary(
+    e.target.nombre.value,
+    e.target.autor.value,
+    e.target.paginas.value,
+    e.target.estado.checked
+  );
+  mostrarLibros()
+  $form.reset()
+  dialog.close();
+};
+
+$form.addEventListener("submit", submit);
